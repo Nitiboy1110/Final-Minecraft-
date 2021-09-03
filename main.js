fd936098698f61a9d19d60de5fd7b07e4d4ce41b
NITYAM <83535754+Nitiboy1110@users.noreply.github.com>
2021-09-03 20:47:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,200 @@
+
+
+var canvas= new fabric.Canvas('my_Canvas');
+
+player_x=10;
+player_y=10;
+
+block_img_width=30;
+block_img_height=30;
+
+var player_object="";
+var block_img_object="";
+
+function player_update()
+{
+    fabric.Image.fromURL("mine+player+lol .png", function (Img)
+        {
+          player_object=Img;
+          player_object.scaleToWidth(150);
+          player_object.scaleToHeight(90);
+          player_object.set({
+              top:player_y,
+              left:player_x
+          });
+          canvas.add(player_object);
+        });       
+}
+
+function new_Image(get_Img)
+{
+    fabric.Image.fromURL(get_Img, function (Img)
+        {
+            block_img_object=Img;
+            block_img_object.scaleToWidth(150);
+            block_img_object.scaleToHeight(90);
+            block_img_object.set({
+              top:player_y,
+              left:player_x
+          });
+          canvas.add(block_img_object);
+        });       
+}
+
+window.addEventListener("keydown" ,my_keydown);
+
+function my_keydown(e)
+{
+  keyPressed = e.keyCode;
+  console.log(keyPressed);
+  if (e.shiftKey == true && keyPressed == '80')
+  {
+    console.log("p and shift pressed together");
+    block_img_width = block_img_width + 10;
+    block_img_height = block_img_height + 10;
+    document.getElementById("current_width").innerHTML = block_img_width;
+    document.getElementById("current_height").innerHTML = block_img_height;
+  }
+  
+  if (e.shiftKey == true && keyPressed == '77')
+  {
+    console.log("m and shift pressed together");
+    block_img_width = block_img_width - 10;
+    block_img_height = block_img_height - 10;
+    document.getElementById("current_width").innerHTML = block_img_width;
+    document.getElementById("current_height").innerHTML = block_img_height;
+  }
+
+  if (keyPressed == '38')
+  {
+    up();
+    console.log("up");
+  }
+
+  if (keyPressed == '40')
+  {
+    down();
+    console.log("down");
+  }
+
+  if (keyPressed == '37')
+  {
+    left();
+    console.log("left");
+  }
+
+  if (keyPressed == '39')
+  {
+    right();
+    console.log("right");
+  }
+
+  if (keyPressed == '87')
+  {
+    new_Image('wall.jpg');
+    console.log("w");
+  }
+
+  if (keyPressed == '71')
+  {
+    new_Image('ground.png');
+    console.log("g");
+  }
+
+  if (keyPressed == '76')
+  {
+    new_Image('light_green.png');
+    console.log("l");
+  }
+
+  if (keyPressed == '84')
+  {
+    new_Image('trunk.jpg');
+    console.log("t");
+  }
+
+  if (keyPressed == '82')
+  {
+    new_Image('roof.jpg');
+    console.log("r");
+  }
+
+  if (keyPressed == '89')
+  {
+    new_Image('yellow_wall.png');
+    console.log("y");
+  }
+
+  if (keyPressed == '68')
+  {
+    new_Image('dark_green.png');
+    console.log("d");
+  }
+
+  if (keyPressed == '85')
+  {
+    new_Image('unique.png');
+    console.log("u");
+  }
+
+  if (keyPressed == '67')
+  {
+    new_Image('cloud.jpg');
+    console.log("c");
+  }
+
+}
+
+
+
+function up()
+{
+   if (player_y>=0)
+   {
+     player_y = player_y - block_img_height;
+     console.log("block img height =" + block_img_height);
+     console.log("When up arrow key is pressed, X =" + player_x + " , Y = " + player_y);
+     canvas.remove(player_object);
+     player_update();
+   }
+   
+}
+
+function down()
+{
+   if (player_y<=500)
+   {
+     player_y = player_y + block_img_height;
+     console.log("block img height =" + block_img_height);
+     console.log("When up arrow key is pressed, X =" + player_x + " , Y = " + player_y);
+     canvas.remove(player_object);
+     player_update();
+   }
+   
+}
+
+function left()
+{
+   if (player_x>0)
+   {
+     player_x = player_x - block_img_width;
+     console.log("block img height =" + block_img_width);
+     console.log("When up arrow key is pressed, X =" + player_x + " , Y = " + player_y);
+     canvas.remove(player_object);
+     player_update();
+   }
+   
+}
+
+function right()
+{
+   if (player_x<=850)
+   {
+     player_x = player_x + block_img_width;
+     console.log("block img height =" + block_img_width);
+     console.log("When up arrow key is pressed, X =" + player_x + " , Y = " + player_y);
+     canvas.remove(player_object);
+     player_update();
+   }
+   
+}
